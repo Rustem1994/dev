b53143769262defe4af4bf1437580f844ef3cb17
Win10Pro <test@test.ru>
2022-03-25 23:07:52 +0300

    26/03/2022

--- src/com.main/test.java
+++ src/com.main/test.java
@@ -1,8 +1,31 @@
 package com.main;
 
+import java.util.Scanner;
+
 public class test {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        if (A>4) {
+            if (A==5) {
+                System.out.println("YES =5");
+            }else{
+                if (A==6) {
+                    System.out.println("YES =6");
+                }else{
+                    System.out.println("NOT 6");
+                }
+            }
+        }else{
+            System.out.println("NOT");
+        }
+
+        switch (A){
+            case 4: System.out.println("4"); break;
+            case 5: System.out.println("5"); break;
+            case 6:{ System.out.println("6");} break;
+        }
     }
 }
